32393fccd63e63e3df948085ef1cdc37cc616bd3
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-29 18:12:41 +0100

    first commit

---
+++ csharp-array-es1/Program.cs
@@ -0,0 +1,14 @@
+﻿// dichiaro la variabile numeroMaggiore e la inizializzo a 0
+int numeroMaggiore = 0;
+// leggo gli argomenti passati in input
+foreach (string arg in args)
+{
+    // se l'argomento è maggiore del numeroMaggiore
+    if (numeroMaggiore < (int.Parse(arg)))
+    {
+        // assegno il valore dell'argomento a numeroMaggiore
+        numeroMaggiore = int.Parse(arg);
+    }
+}
+// stampo il numero maggiore
+Console.WriteLine($"Il numero più grande è: {numeroMaggiore}");
